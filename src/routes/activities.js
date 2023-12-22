38cdc6523d7b3fa4de45d0559377620bc93b243d
suba <thanikaikumaransuba@gmail.com>
2023-12-22 22:26:06 +0530

    test

--- src/routes/activities.js
+++ src/routes/activities.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { getActivities, createActivity, getActivity, updateActivity, deleteActivity } = require('../controllers/activity.controller')
 const validateToken = require('../middlewares/validateTokenHandler')
-
+// validation
 router.use(validateToken)
 router.route('/').get(getActivities).post(createActivity)
 router.route('/:id').get(getActivity).put(updateActivity).delete(deleteActivity)
